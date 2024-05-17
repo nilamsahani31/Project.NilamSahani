873479a02939ae866c2cef63347679edca9a1f05
nilamsahani31 <166593461+nilamsahani31@users.noreply.github.com>
2024-05-17 13:59:10 +0530

    Delete Hotel directory

--- Hotel/src/main/java/com/guru/Hotel/DAO/BookTableRepository.java
+++
@@ -1,9 +0,0 @@
-package com.guru.Hotel.DAO;
-
-
-import com.guru.Hotel.Entity.BookTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookTableRepository extends JpaRepository<BookTable, String> {
-
-}
--- Hotel/src/main/java/com/guru/Hotel/Service/BookTableService.java
+++
@@ -1,9 +0,0 @@
-package com.guru.Hotel.Service;
-
-import com.guru.Hotel.Entity.BookTable;
-
-public interface BookTableService {
-
-    BookTable addTable(BookTable bookTable) throws Exception;
-
-}
